5ef9ba90cb58b19233b6d111648345bd372487eb
amir <amir800900@mail.ru>
2024-11-19 16:14:37 +0300

    fix front

--- SelectedProducts/selectedProducts.py
+++ SelectedProducts/selectedProducts.py
@@ -1,7 +1,13 @@
 from flask import render_template
 
+from ServiceFiles.links import header_links
+
 
 class SelectedProduct:
     @staticmethod
     def show_selected_product_page():
-        return render_template("SelectedProducts/selected_product.html")
+        return render_template(
+            "SelectedProducts/selected_product.html",
+            header_links=header_links,
+            title="Выбор блюда"
+        )
