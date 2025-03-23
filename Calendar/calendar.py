64b7dfe3754e79c3729030cec5cab399330f8175
mihaillepenkin <mihaillepenkin@gmail.com>
2025-03-23 20:55:11 +0300

    2+2=5

--- Calendar/calendar.py
+++ Calendar/calendar.py
@@ -1,14 +1,38 @@
-from flask import render_template
+from flask import render_template, session, request
+from datetime import datetime, date
 
 from ServiceFiles.links import header_links
+import DataBase
+from settings import app
 
 
 class Calendar:
     @staticmethod
     def show_calendar_page():
-        return render_template(
-            "Calendar/calendar.html",
-            header_links=header_links,
-            title="Календарь",
-            ca_is_active="active"
+        if "user_id" in session:
+            user_id = session["user_id"][0][0]
+            user_in_all_time = DataBase.use_DataBase.get_all_day_data(user_id)
+            print(user_in_all_time)
+            return render_template(
+                "Calendar/calendar.html",
+                header_links=header_links,
+                title="Календарь",
+                ca_is_active="active",
+                cookies=user_in_all_time
             )
+        return "You are not logged in", 401
+
+    def show_calendar_page_with_id(us_id):
+            user_id = us_id
+            user_in_all_time = DataBase.use_DataBase.get_all_day_data(user_id)
+            print(user_in_all_time)
+            return render_template(
+                "Calendar/calendar.html",
+                header_links=header_links,
+                title="Календарь",
+                ca_is_active="active",
+                cookies=user_in_all_time
+            )
+    @app.route("/calendar/<int:student_id>")
+    def calendar_page(student_id):
+        return Calendar.show_calendar_page_with_id(student_id)
--- Trainer/trainer_students.py
+++ Trainer/trainer_students.py
@@ -1,32 +1,50 @@
-from flask import render_template
+from flask import render_template, session
 
+from DataBase.use_DataBase import *
 from ServiceFiles.links import header_links
+from settings import app
 
 
 class Students:
+
     @staticmethod
     def show_students_page():
-        return render_template(
-            "Trainer/students.html",
-            header_links=header_links,
-            title="Мои студенты"
-        )
+        if "user_id" in session:
+            user_id = session["user_id"][0][0]
+            if check_trener(user_id):
+                student_list = get_list_of_student(user_id)
+                students_info = []
+                for el in student_list:
+                    students_info.append(get_user_data(el))
+                return render_template(
+                    "Trainer/students.html",
+                    header_links=header_links,
+                    title="Мои студенты",
+                    cookies=students_info
+                )
+            return "You are not trener", 401
+        return "You are not logged in", 401
 
     @staticmethod
     def show_one_student_page(student_id):
-        student_title = "Имя чела"
-        student_image_url = "https://via.placeholder.com/800x400"
-        student_content = """
-                <p>Я знаю, что id этой странички""" + str(student_id) + """</p>
-                <p>Здесь будет текст. Это пример текста, который может быть</p>
-                <p>Здесь будет текст. Это пример текста, который может быть</p>
-                <p>Здесь будет текст. Это пример текста, который может быть</p>
-                """
+        user_info = get_user_data(student_id)
+        student_title = user_info["name"]
+        student_image_url = user_info["avatar"]
+        student_content = (f"Возраст: {user_info["age"]}"
+                           f"Пол: {user_info["sex"]}"
+                           f"Почта {user_info["email"]}"
+                           )
+        print(user_info)
         return render_template(
             "Trainer/student.html",
-            title=f"Мое имя {str(student_title)}, а мой id-{str(student_id)}",
+            title=f"Ученик {user_info["name"]}",
             header_links=header_links,
             student_title=student_title,
             student_image_url=student_image_url,
-            student_content=student_content
+            student_content=student_content,
+            cookies=user_info
         )
+
+    @app.route("/student/<int:student_id>")
+    def student_page(student_id):
+        return Students.show_one_student_page(student_id)
