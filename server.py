75e6ae24ae4a7b75fdf784a05cc1b3ebfcde3f03
Amir <amir800900@mail.ru>
2025-03-30 16:18:06 +0300

    Пофиксил вёрстку авторизации и регистрации, подправил импорты в server.py, исправил баг в календаре.

--- server.py
+++ server.py
@@ -1,8 +1,5 @@
 import os
 
-from flask import request, render_template
-from utils import render_template_with_user
-
 from server.hendler.handler import Handler
 from server.admin.admin import Admin
 from server.calendar.calendar import Calendar
@@ -13,16 +10,11 @@ from server.login.registration import Registration
 from server.main_page.main_page import StartPage
 from server.news.news import News
 from server.selected_products.selectedProducts import SelectedProduct
-from server.service_files.links import main_page, admin, calendar, day_plan, selected_products, registration, \
-    authorization, \
-    cabinet, all_news, one_news, add_product, save_day_plan, process_registration, confirm_code, process_login, logout, \
-    add_product, all_students, student, make_news, save_news
 from server.trainer.trainer_students import Students
 from server.food_controler.food_controller import food_blueprint, delete_old_diets
 from apscheduler.schedulers.background import BackgroundScheduler
-from flask import request, render_template
 from settings import app, host
-from server.service_files.links import *
+
 
 app.secret_key = os.urandom(24)
 app.register_blueprint(food_blueprint)
