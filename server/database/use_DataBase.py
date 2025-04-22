26ceb862f56043547818766446ade74b758f809b
jaruccky <qdqdqdq3213212313@gmail.com>
2025-04-22 17:03:53 +0300

    chat trainer finished

--- server/database/use_DataBase.py
+++ server/database/use_DataBase.py
@@ -3,6 +3,7 @@ import psycopg2
 from settings import host
 from settings import user, password, db_name, port
 
+from datetime import datetime
 
 def database_query(sql, fetch=False):
     connection = []
@@ -137,27 +138,26 @@ def get_list_of_student(user_id):
 
 
 def get_message_history(first_id, second_id, since=None):
+    
     if since:
         sql = f"""
-        SELECT id_from, id_to, content, time
+        SELECT id_from, id_to, content, message_time
         FROM messages
         WHERE ((id_from = {first_id} AND id_to = {second_id}) 
                OR (id_from = {second_id} AND id_to = {first_id}))
-        AND time > '{since}'
-        ORDER BY time
+        AND message_time > '{since}'
+        ORDER BY message_time
         """
     else:
         sql = f"""
-        SELECT id_from, id_to, content, time
+        SELECT id_from, id_to, content, message_time
         FROM messages
         WHERE (id_from = {first_id} AND id_to = {second_id}) 
                OR (id_from = {second_id} AND id_to = {first_id})
-        ORDER BY time
+        ORDER BY message_time
         """
 
     data = database_query(sql, True)
-    print(data)
-    print(database_query("SELECT * FROM messages", True))
     message_history = []
     for el in data:
         message = {"id_from": el[0], "id_to": el[1], "content": el[2], "time": el[3]}
@@ -167,7 +167,7 @@ def get_message_history(first_id, second_id, since=None):
 
 def add_message(id_from, id_to, content):
     sql = f"""
-    INSERT INTO messages (id_from, id_to, content, time)
+    INSERT INTO messages (id_from, id_to, content, message_time)
     VALUES ({id_from}, {id_to}, '{content}', NOW())
     """
     return database_query(sql)
