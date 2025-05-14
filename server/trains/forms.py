63761ac92880665556c53938adb4f3e15cf538a2
vair <volodina.vn@phystech.edu>
2025-05-14 12:00:38 +0300

    added get_user_train

--- server/trains/forms.py
+++ server/trains/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, DateField, StringField
 from wtforms.validators import DataRequired, NumberRange
 
-class TrainForm(FlaskForm):
+class AddTrainForm(FlaskForm):
     train_id = IntegerField(name = "integer_id", validators=[DataRequired])
-    date = DateField(name = "date", validators=[DataRequired])
     time = StringField(name = "time", validators=[DataRequired, NumberRange(min=1)])
+    date = DateField(name = "date", validators=[DataRequired])
