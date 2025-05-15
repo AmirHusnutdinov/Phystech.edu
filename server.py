aaf63a4e948a09de06d86ebae51dc37bbbf875fd
jaruccky_ft <qdqdqdq3213212313@gmail.com>
2025-05-15 17:20:15 +0300

    port

--- server.py
+++ server.py
@@ -27,5 +27,5 @@ scheduler.add_job(delete_old_diets, "cron", hour=0, minute=0)
 scheduler.start()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
