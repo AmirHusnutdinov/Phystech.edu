8f37dc08cd6d95350110a5e636cc1d15e9e75184
mihaillepenkin <mihaillepenkin@gmail.com>
2025-05-15 15:13:42 +0300

    addPhysicsInCalendar

--- server.py
+++ server.py
@@ -25,5 +25,6 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(delete_old_diets, "cron", hour=0, minute=0)
 scheduler.start()
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host=host, port=port, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
