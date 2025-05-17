de8194a3cf2d19abaee0c2be42c9a00cfcb35197
mihaillepenkin <mihaillepenkin@gmail.com>
2025-05-17 17:25:35 +0300

    changeServer

--- server.py
+++ server.py
@@ -18,9 +18,7 @@ from server.news.news import News
 from server.selected_products.selectedProducts import SelectedProduct
 from server.service_files.links import *;
 from server.trainer.trainer_students import Students
-from settings import app as apchy
-
-app = apchy
+from settings import app
 
 app.secret_key = os.urandom(24)
 app.register_blueprint(food_blueprint)
