accbe5bc200470b8834ceeb814a101d49203c8c4
mihaillepenkin <mihaillepenkin@gmail.com>
2025-05-17 17:09:06 +0300

    changeServer

--- server.py
+++ server.py
@@ -232,5 +232,5 @@ def get_new_messages(student_id):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
