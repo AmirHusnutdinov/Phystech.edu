96aaaca09c1797997268f860d8c667a566aff603
mihaillepenkin <mihaillepenkin@gmail.com>
2025-05-18 02:07:42 +0300

    fixBags#3

--- server/database/use_DataBase.py
+++ server/database/use_DataBase.py
@@ -228,6 +228,7 @@ def get_list_of_student(user_id):
 
 def add_student(trainer_id, student_id):
     sql = f"INSERT INTO student (id, student_id) VALUES ({trainer_id}, {student_id})"
+    database_query(sql)
 
 
 def get_message_history(first_id, second_id, since=None):
