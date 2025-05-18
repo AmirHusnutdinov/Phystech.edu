c2b95677e8957e8516f1510756e8bd1cb4a9b80f
mihaillepenkin <mihaillepenkin@gmail.com>
2025-05-18 12:16:15 +0300

    addRegistrationInstruction

--- server/main_page/main_page.py
+++ server/main_page/main_page.py
@@ -1,4 +1,5 @@
 from server.cloud.cloud_main import Cloud
+from server.login.forms import RegistrationForm, LoginForm
 from server.service_files.links import *
 from settings import app
 from utils import render_template_with_user
@@ -16,12 +17,16 @@ class StartPage:
             header_links = choose_header_links("not-authorized")
         
         images = StartPage.cloud_client.get_folder("images/")
+        form = LoginForm()
+        RegistrationForm()
 
         return render_template_with_user(
             "MainPage/main.html",
             title="Главная страница",
             images=images,
             header_links=header_links,
+            form=form,
+            form_registration=form
         )
 
 
